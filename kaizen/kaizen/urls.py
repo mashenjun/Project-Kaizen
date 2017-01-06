e419ef18f01336c0d5aedf9cb0bbd484d543f1fb
mashenjun <mashenjun@sina.cn>
2017-01-07 05:32:27 +0800

    test restful framwork

--- kaizen/kaizen/urls.py
+++ kaizen/kaizen/urls.py
@@ -15,9 +15,18 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework import routers
+from testrestful import views
 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^$', 'home.views.index'),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
-    url(r'^testconnect/', include('testconnect.urls')),
+    url(r'^testconnect/', include('testconnect.urls'))
 ]
