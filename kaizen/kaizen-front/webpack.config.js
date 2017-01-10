e72c4026b89583d8db7cb2d90c82a6f945662e00
Rendong Liu <rendong_liu@hotmail.com>
2017-01-10 15:43:41 +0800

    change config

--- kaizen/kaizen-front/webpack.config.js
+++ kaizen/kaizen-front/webpack.config.js
@@ -18,18 +18,20 @@ var config = {
     },
     module: {
         loaders: [{
-            test: /\.jsx?$/,
-            include: APP_DIR,
-            exclude: /(node_modules|bower_components|dist)/,
-            loader: "babel"
-        }]
+                test: /\.jsx?$/,
+                include: APP_DIR,
+                exclude: /(node_modules|bower_components|dist)/,
+                loader: "babel"
+            },
+            { test: /\.less$/,   loader: "style-loader!css-loader!less-loader" },
+        ]
     },
-     devServer: {
-	    publicPath: '/dist',
-	    filename: 'bundle.js',
-	    port: 8081,
-	    host: '0.0.0.0'
-  	}
+    devServer: {
+        publicPath: '/dist',
+        filename: 'bundle.js',
+        port: 8081,
+        host: '0.0.0.0'
+    }
 }
 
 module.exports = config;
