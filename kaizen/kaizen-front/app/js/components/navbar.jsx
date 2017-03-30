91be55e77a4b5f16b0a34bcf1389f971c5ae9a2e
Rendong Liu <rendong_liu@hotmail.com>
2017-03-31 02:13:56 +0800

    add city selection

--- kaizen/kaizen-front/app/js/components/navbar.jsx
+++ kaizen/kaizen-front/app/js/components/navbar.jsx
@@ -18,6 +18,8 @@ class NavbarComponent extends Component {
       startDate: moment(),
       src: "",
       cropResult: "",
+      provinces: [],
+      citys:[],
     };
   }
 
@@ -29,6 +31,22 @@ class NavbarComponent extends Component {
     }
   };
 
+  onProvinchange = ()=>{
+    fetch('/upload/query/province/'+this.proveList.value).then((response) => response.json()).then((data) => {
+      this.setState({citys: data})
+    });
+  };
+
+  onCitychange = ()=>{
+
+  };
+
+
+  componentDidMount = () => {
+    fetch('/upload/query/province/').then((response) => response.json()).then((data) => {
+      this.setState({provinces: data})
+    });
+  };
 
   onChange = (e) => {
     e.preventDefault();
@@ -45,21 +63,12 @@ class NavbarComponent extends Component {
     reader.readAsDataURL(files[0]);
   };
 
-  _crop = () => {
-    if (typeof this.cropper.getCroppedCanvas() === 'undefined') {
-      return;
-    }
-    this.setState({
-      cropResult: this.cropper.getCroppedCanvas().toDataURL(),
-    });
-  };
-
-  onFetchDatahandler = (data)=>{
+  onFetchDatahandler = (data) => {
     const postUrl = '/testrestful/OSSpage/';
-    window.location.href = postUrl+ "?uploaderid=" + data.id;
+    window.location.href = postUrl + "?uploaderid=" + data.id;
   };
 
-  onFetchErrorHandler = (err)=>{
+  onFetchErrorHandler = (err) => {
     console.log(err);
   };
 
@@ -67,17 +76,16 @@ class NavbarComponent extends Component {
     const {uid} = localstore.getToken();
     let formData = new FormData();
     formData.append('name', this.name.value);
-    // formData.append('birth_day', this.state.startDate.format());
-    formData.append('birth_day', '2012-03-08T00:12')
+    formData.append('birth_day', this.state.startDate.format('YYYY-MM-DD'));
     formData.append('sex', this.sex.value);
     formData.append('user', uid);
     formData.append('home_town', 'shanghai');
-    formData.append('location', [11,22]);
+    formData.append('location', [11, 22]);
     if (!this.cropper.getCroppedCanvas() || typeof this.cropper.getCroppedCanvas() === 'undefined') {
-        fetch('/upload/uploader/', {
-          method: 'POST',
-          body: formData
-        }).then((response)=>response.json()).then(this.onFetchDatahandler).catch(this.onFetchErrorHandler);
+      fetch('/upload/uploader/', {
+        method: 'POST',
+        body: formData
+      }).then((response) => response.json()).then(this.onFetchDatahandler).catch(this.onFetchErrorHandler);
     } else {
       this.cropper.getCroppedCanvas().toBlob((blob) => {
         formData.append('photo', blob);
@@ -132,11 +140,20 @@ class NavbarComponent extends Component {
           </div>
       )
     }
-    console.log('rererer...rerender', this.props.userUploaders);
+    console.log('rererer...rerender');
     const {username, isAuthenticated} = localstore.getToken();
     if (uploaderList.length === 0) {
       uploaderList = 'You do not have any uploaders. You should create one'
     }
+    const provinceList = [];
+    for (let p of this.state.provinces) {
+      provinceList.push(<option key={p.code} value={p.code}>{p.name}</option>);
+    }
+    const cityList = [];
+    for (let c of this.state.citys) {
+      cityList.push(<option key={c.code} value={c.code}>{c.name}</option>);
+    }
+
     return (
         <div>
           <div ref={(modal) => this.modal = modal} className="modal">
@@ -212,7 +229,9 @@ class NavbarComponent extends Component {
                         <div className="field">
                           <div className="control">
                              <span className="select">
-                              <select ref={(sex)=>{this.sex=sex}}>
+                              <select ref={(sex) => {
+                                this.sex = sex
+                              }}>
                                 <option value={'M'}>Male</option>
                                 <option value={'F'}>Female</option>
                               </select>
@@ -231,7 +250,7 @@ class NavbarComponent extends Component {
                           <div className="control">
                             <input type="file" accept="image/*" onChange={this.onChange}/>
                             <Cropper
-                                style={{maxHeight: 150, maxWidth: 150}}
+                                style={{maxHeight: 180, maxWidth: 180}}
                                 aspectRatio={1}
                                 preview=".img-preview"
                                 guides={false}
@@ -263,6 +282,7 @@ class NavbarComponent extends Component {
                         </div>
                       </div>
                     </div>
+
                     <div className="field is-horizontal">
                       <div className="field-label is-normal">
                         <label className="label">HomeTown</label>
@@ -270,17 +290,35 @@ class NavbarComponent extends Component {
                       <div className="field-body">
                         <div className="field">
                           <div className="control">
-                            <input className="input" type="text" placeholder="Home town"/>
+                            <span style={{marginLeft: '8px', lineHeight: '2.5', marginRight: '8px'}}>Province:</span>
+                            <span className="select">
+                                <select onChange={this.onProvinchange} ref={(proveList)=>{this.proveList = proveList}}>
+                                  <option>---</option>
+                                  {provinceList}
+                                </select>
+                               </span>
+                            <span style={{marginLeft: '8px', lineHeight: '2.5',marginRight: '8px'}}>City:</span>
+                            <span className="select">
+                                <select onChange={this.onCitychange} ref={(cityList)=>{this.cityList = cityList}}>
+                                  <option>---</option>
+                                  {cityList}
+                                </select>
+                               </span>
                           </div>
                         </div>
                       </div>
                     </div>
+
+
                   </div>
                 </section>
                 <footer className="modal-card-foot">
                   <a className="button is-success" onClick={this.onCreateHandler}><i className="fa fa-plus"
                                                                                      style={{marginRight: '5px'}}></i>Create</a>
-                  <a className="button" onClick={() => this.formModal.classList.remove("is-active")}>Cancel</a>
+                  <a className="button" onClick={() => {
+                    this.formModal.classList.remove("is-active");
+                    window.location.reload();
+                  }}>Cancel</a>
                 </footer>
               </div>
             </div>
