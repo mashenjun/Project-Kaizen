a5b80ddc6ff34b02062aff992b53e9c020aaabc1
Shenjun Ma <mashenjun0902@gmail.com>
2017-04-01 17:51:03 +0200

    change setting

--- kaizen/kaizen/settings.py
+++ kaizen/kaizen/settings.py
@@ -24,16 +24,17 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '=mq6_9!h*=1r)u-gnb-yia6p9(xn7b46&fyq8ce0f+rs1p6rkz'
+DEBUG = True
 try:
-    from kaizen.config import SECRET_KEY
+    from kaizen.config import SECRET_KEY, DEBUG
 
     SECRET_KEY = SECRET_KEY
-
+    DEBUG = DEBUG
 except:
     pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = ['47.92.27.34','localhost',]
 
