1f2b60d327fb2c81d13d37f6f821bd8e24e49d3d
Shenjun Ma <mashenjun0902@gmail.com>
2017-11-21 10:44:32 +0100

    .

--- kaizen/prepareBackend.sh
+++ kaizen/prepareBackend.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # exec ~/Project-Kaizen/kaizen/manage.py runserver 0.0.0.0:8080
+source ~/Project-Kaizen/env/bin/activate
+rm -rf ~/Project-Kaizen/kaizen/static/
 exec ~/Project-Kaizen/kaizen/manage.py collectstatic --noinput
 echo "collect statics finish!"
